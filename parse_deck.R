51639089070a17d7b993675c37425523123d4c75
Joshua Schill <69824228+J-SCHILL@users.noreply.github.com>
2023-09-19 14:52:15 -0500

    Update parse_deck.R

--- parse_deck.R
+++ parse_deck.R
@@ -1,10 +1,12 @@
-#deck optimizer
-
+#load packages
 library(readxl)
 library(tidyverse)
 
-deck = read_xlsx( "C:\\Users\\xjxs528\\OneDrive - W.W Grainger, inc\\Desktop\\lugia_ss.xlsx", col_names = F)
-deck
+#paste a decklist into excel and import
+#(in the future set up an import straight from clipboard)
+deck = read_xlsx( file.choose(), col_names = F)
+
+#regex format
 regexp = ' (?=[^ ]+$)'
 
 split1 <- do.call(rbind, strsplit(deck[[1]], regexp, perl=TRUE))
@@ -15,10 +17,10 @@ split2 <- do.call(rbind, strsplit(deck[[1]], regexp, perl=TRUE))
 deck$set <- split2[,2]
 deck$...1 <- split2[,1]
 
-rexp <- "^(\\w+)\\s?(.*)$"
+regexp <- "^(\\w+)\\s?(.*)$"
 
-deck$count <- sub(rexp,"\\1",deck$...1)
-deck$mons = sub(rexp,"\\2",deck$...1)
+deck$count <- sub(regexp,"\\1",deck$...1)
+deck$mons = sub(regexp,"\\2",deck$...1)
 
 clean_deck <- deck %>% filter(mons != "") %>%
   transmute(count = as.double(count), mons, set, coll_id)
@@ -26,12 +28,5 @@ clean_deck <- deck %>% filter(mons != "") %>%
 count <- clean_deck$count %>% sum()
 
 
-#function to calculate odds of drawing card X off the top
-draw = function(deck, card){
-  totalx = filter(deck, mons == card) %>% pull(count) %>% sum()
-
-  totalx/(clean_deck$count %>% sum())
-
-}
 clean_deck %>% mutate(draw_prob = count/60)
 
